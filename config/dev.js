9c2666cddd08643d6f055c71fa7d90d894534d1f
Bolu <badegbemile@gmail.com>
2020-05-22 18:31:34 +0100

    Removed keys from git

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-const API_FOOTBALL_URI = "https://api-football-v1.p.rapidapi.com/v2";
-const API_FOOTBALL_HOST = "api-football-v1.p.rapidapi.com";
-
-module.exports = {
-    API_FOOTBALL_URI,
-    API_FOOTBALL_HOST,
-}
--- config/keys.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = 'AA577TkE5YmshvJvVL5KK2NVYXudp1MsfZnjsnlzCEypeXIdhe';
-
-module.exports = {
-    API_KEY,
-};
